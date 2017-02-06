725587c17de100ee6635cd715908adcba99c954d
RainbowsNPonies <kenicia7@hotmail.com>
2017-02-06 11:55:55 -0400

    Created class implimentation file for Student.h

---
+++ Student.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Student.h"
+
+
+    void Student::setGrade(char g){
+
+      	grade = g;
+
+    }
+
+    char Student::getGrade() {
+	      return grade;
+    }
+
+};
+
+#endif
