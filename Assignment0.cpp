36f235dacaf619d9b0f594dc36c9037205f3a6c8
RainbowsNPonies <kenicia7@hotmail.com>
2017-02-08 17:33:23 -0800

    Corrected code to truly answer assignment

--- Assignment0.cpp
+++ Assignment0.cpp
@@ -1,18 +1,37 @@
 #include <iostream>
 #include <iomanip>
-#include "Student.h" 
+#include <vector>
 using namespace std;
+#include "Student.h"
 
 int main() {
-
-	char grade = ' ' ;
-
-	Student student;
 	
-	cout << "Please enter a student grade: ";
-	cin >> grade;
-	student.setGrade(grade);
-	cout << student.getGrade(); 
+    int i;
+	
+    Student grade1;
+    Student grade2;
+    Student grade3;
+    Student grade4;
+
+    grade1.setGrade('A');
+    grade2.setGrade('D');
+    grade3.setGrade('F');
+    grade4.setGrade('B');
+
+    vector<Student>Grades;
+	
+	Grades.push_back(grade1);
+	Grades.push_back(grade2);
+	Grades.push_back(grade3);
+	Grades.push_back(grade4);
+
+    cout << "Student Grades: "<<endl;
+
+    for (i=0;i<Grades.size();i++){
+        cout << Grades[i].getGrade() <<endl;
+    }
+
 
 return 0;
 }
+
