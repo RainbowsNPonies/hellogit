544ef185604199c2425494362016a021d69d035d
RainbowsNPonies <kenicia7@hotmail.com>
2017-02-08 17:31:09 -0800

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,19 +1,11 @@
 #ifndef STUDENT_H
 #define STUDENT_H
-#include <iostream>
-
-#include <iomanip>
-
-
 
 class Student {
-
     private:
     char grade = ' ';
 	
-
     public:
-
     void setGrade(char g);
     char getGrade();
 	    
