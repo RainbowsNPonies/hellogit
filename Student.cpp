7c026c44bbd176490baf36cde2c55d60dd42731a
RainbowsNPonies <kenicia7@hotmail.com>
2017-02-08 17:29:46 -0800

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -1,17 +1,12 @@
-#include <iostream>
 #include "Student.h"
 
 
     void Student::setGrade(char g){
-
       	grade = g;
-
     }
 
     char Student::getGrade() {
 	      return grade;
     }
 
-};
 
-#endif
